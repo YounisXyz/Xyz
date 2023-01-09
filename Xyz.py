28c6d76c22496828734df5f5f606cef9b65b818a
SACRED GAMES 3.06 <106374961+YounisJohn@users.noreply.github.com>
2023-01-09 09:51:52 +0500

    Create Xyz.py

---
+++ Xyz.py
@@ -0,0 +1,48 @@
+import os
+
+ 
+
+ 
+
+ 
+
+if __name__ == "__main__":
+
+ 
+
+ 
+
+ 
+
+   try:
+
+ 
+
+ 
+
+ 
+
+       os.system("git pull")
+
+ 
+
+
+ 
+
+       __import__("Younisxyz").XYOUNISJOHN()
+
+ 
+
+ 
+
+ 
+
+   except Exception as e: 
+
+ 
+
+ 
+
+ 
+
+       exit(str(e))
