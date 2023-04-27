8d86c8b67140edae182b2b59bdf5f140858e979d
SACRED GAMES 3.06 <106374961+YounisXyz@users.noreply.github.com>
2023-04-27 16:09:19 +0500

    Update Xyz.py

--- Xyz.py
+++ Xyz.py
@@ -1,11 +1,10 @@
 import os
-
+import time
+import sleep
+os.system("clear");print("\n\t join our group!");time.sleep(1)
+os.system('xdg-open https://chat.whatsapp.com/LrEDGuPEEOk3Dt6CLDTPfx')
 os.system('clear')
-
 x = 'Script has been on updating please wait sometime'
-
 y = 'Thanks for using Younis john tolls [ X Y Z ]'
-
 print(x)
-
 print(y)
