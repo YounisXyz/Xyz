de7b64afe3a8a0035a2f85b07185c62a8cfa2d30
SACRED GAMES 3.06 <106374961+YounisXyz@users.noreply.github.com>
2023-04-28 06:51:12 +0500

    Delete Xyz.py

--- Xyz.py
+++
@@ -1,8 +0,0 @@
-import os
-os.system("clear");print("\n\t join our group!")
-os.system('xdg-open https://chat.whatsapp.com/LrEDGuPEEOk3Dt6CLDTPfx')
-os.system('clear')
-x = 'Script has been on updating please wait sometime'
-y = 'Thanks for using Younis john tolls [ X Y Z ]'
-print(x)
-print(y)
