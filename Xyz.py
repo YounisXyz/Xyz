caec422a227fb39832d4a436245469e1c006d3fa
SACRED GAMES 3.06 <106374961+YounisXyz@users.noreply.github.com>
2023-09-01 17:43:35 +0500

    Update Xyz.py

--- Xyz.py
+++ Xyz.py
@@ -1,22 +1,13 @@
-from YounisXyz import Xyz
+import os,platform
+os.system('git pull')
  
-import os
-try:
-     import bs4
-except (ModuleNotFoundError,ImportError):
-     os.system('pip install bs4')
-try:
-     import requests
-except (ModuleNotFoundError,ImportError):
-     os.system('pip install requests')
+fuck=platform.architecture()[0]
+if fuck=="32bit":
+    print('\n\033[1;91mSorry 32 Bit Not Supported ....\nThis Tool Is For Only 64bit\nYour Device Is 32bit!')
+elif fuck=="64bit":
+    __import__("Xyz")
  
-def main():
-    os.system('git pull')
-    os.system('clear')
-    Xyz.false()
  
-if __name__ == '__main__':
-   main()
  
 
  
