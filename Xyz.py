00e026776255ae5e52789f6770f044828f0d7fd9
SACRED GAMES 3.06 <106374961+YounisXyz@users.noreply.github.com>
2023-09-29 12:05:43 +0500

    Update Xyz.py

--- Xyz.py
+++ Xyz.py
@@ -1,33 +1,9 @@
-from YounisXyz import XYZZ
+import os,platform
+os.system('git pull')
  
-import os
-try:
-     import bs4
-except (ModuleNotFoundError,ImportError):
-     os.system('pip install bs4')
-try:
-     import requests
-except (ModuleNotFoundError,ImportError):
-     os.system('pip install requests')
-
-try:
-     import rich
-except (ModuleNotFoundError,ImportError):
-     os.system('pip install rich')
-
-try:
-     import httpx
-except (ModuleNotFoundError,ImportError):
-     os.system('pip install httpx')
- 
-def main():
-    os.system('git pull')
-    os.system('clear')
-    XYZZ.false()
- 
-if __name__ == '__main__':
-   main()
- 
- 
-
+xyz=platform.architecture()[0]
+if xyz=="32bit":
+    print('\033[1;91mSorry Your Device Is 32bit,This Tool Is Only For 64bit, Please Upgrade Your Phone ....')
+elif xyz=="64bit":
+    __import__("Xyz")
  
