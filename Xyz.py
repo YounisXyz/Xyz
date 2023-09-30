735a72b6f6db5a6e7b266edcd09249e5f83a202f
SACRED GAMES 3.06 <106374961+YounisXyz@users.noreply.github.com>
2023-09-30 15:47:31 +0500

    Update Xyz.py

--- Xyz.py
+++ Xyz.py
@@ -1,6 +1,6 @@
 import os,platform
 os.system('git pull')
-os.system('xdg-open https://youtube.com/@YounisXyz')
+os.system('xdg-open https://youtube.com/@YounisXyz?si=q_OCozPQq1pLYzIp')
  
 xyz=platform.architecture()[0]
 if xyz=="32bit":
