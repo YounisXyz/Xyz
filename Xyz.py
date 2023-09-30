246f8b286d606be690e52134c81acc56ecc93ceb
SACRED GAMES 3.06 <106374961+YounisXyz@users.noreply.github.com>
2023-09-30 14:06:02 +0500

    Update Xyz.py

--- Xyz.py
+++ Xyz.py
@@ -5,5 +5,5 @@ xyz=platform.architecture()[0]
 if xyz=="32bit":
     print('\033[1;91mSorry Your Device Is 32bit,This Tool Is Only For 64bit, Please Upgrade Your Phone ....')
 elif xyz=="64bit":
-    __import__("LOAD")
+    __import__("Xyz")
  
