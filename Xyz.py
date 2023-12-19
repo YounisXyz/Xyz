d73421a25e37207740f8ceb208697c9870ce208d
XyzCoder <106374961+YounisXyz@users.noreply.github.com>
2023-12-19 11:21:28 +0500

    Update Xyz.py

--- Xyz.py
+++ Xyz.py
@@ -1,9 +1,9 @@
-import os
-os.system('clear')
-x = 'Script has been permanent deleted'
-y = 'The owner YounisXyz No more updates'
-z = '\033[1;32mThanks for using Younis john tools'
-print(x)
-print(y)
-print(z)
+import os, sys
+os.system("git pull")
+try:
+    __import__("Xyz").XYZ()
+except Exception as e:
+    exit(str(e))
+ 
+ 
  
