19730868dbca467dcc5d4e6b07a1bb9ab832ade7
XyzCoder <106374961+YounisXyz@users.noreply.github.com>
2024-03-14 13:47:59 +0500

    Update Xyz.py

--- Xyz.py
+++ Xyz.py
@@ -2,8 +2,8 @@ import os,re,sys,platform
 os.system('git pull')
 bit = platform.architecture()[0]
 if bit == '64bit':
-    from YounisXyz import XYZ
-    XYZ()
+    from YounisXyz import YOUNISXD
+    YOUNISXD()
 elif bit == '32bit':
     exit('\033[1;91mSorry This Tools Not Working on 32 Bit Device')
  
